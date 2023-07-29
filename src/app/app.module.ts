2bd9a4980cc070a09fb43315faf15bee1cea4abf
reotsugua <renanaugusto.ras@gmail.com>
2023-07-29 18:58:10 -0300

    component e module star

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StarComponent } from './shared/component/star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
